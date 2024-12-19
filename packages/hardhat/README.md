bd994c1b8ff9cd9706f040b980c8baa55ecad8eb
jeffIshmael <jeffishmael141@gmail.com>
2024-12-19 19:39:08 +0300

    first commit

---
+++ packages/hardhat/README.md
@@ -0,0 +1,8 @@
+## LocalBuzz smart Contract
+
+<ul>
+<li><a href="https://celoscan.io/address/0x32Dd30a57A909290CF7127A77438dABE373a95a7#code">Mainnet</a></li>
+<li><a href="https://alfajores.celoscan.io/address/0xE11E029eFc6bB08DdF9Ecdc4af6d1047F9488A20#code">Testnet</a></li>
+</ul>
+
+
---
+++ packages/react-app/providers/Blockchain-Providers.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import {   hederaTestnet } from "wagmi/chains";
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [injectedWallet],
+    },
+  ],
+  {
+    appName: "CarbonTrace",
+    projectId: "96d07018513129235d7af00f1cc9fcdb",
+  },
+);
+
+export const config = createConfig({
+  connectors,
+  // chains: [optimism],
+  chains: [hederaTestnet],
+  transports: {
+    [hederaTestnet.id]: http(),
+  },
+});
+
+export function BlockchainProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const queryClient = new QueryClient();
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
